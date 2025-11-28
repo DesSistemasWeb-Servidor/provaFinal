58f089b01b70a1d2148c0023167dcddcdc93e3eb
GiovannaKarolline <mennezes.giovanna@gmail.com>
2025-11-28 20:23:22 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ from flask import make_response
 from flask import redirect, url_for, flash, session
 from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, PasswordField
+from wtforms import StringField, SubmitField, SelectField, PasswordField, TextAreaField
 from wtforms.validators import DataRequired
 import os
 from flask_sqlalchemy import SQLAlchemy
@@ -44,72 +44,38 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-class NameForm(FlaskForm):
-    name = StringField("What is your name?", validators = [DataRequired()])
-    role = SelectField("Role?:", choices = [('Administrator', 'Administrator'), ('Moderator', 'Moderator'), ('User', 'User')], validators = [DataRequired()])
-    submit = SubmitField('Submit')
+class Curso(db.Model):
+    __tablename__ = 'cursos'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(64), unique=True, index=True)
+    descricao = db.Column(db.String(250), index=True)
 
-class LoginForm(FlaskForm):
-    usuario = StringField('Usuário ou e-mail', validators = [DataRequired()], render_kw={"placeholder": "Usuário ou e-mail"})
-    senha = PasswordField('Informe a sua senha', validators = [DataRequired()], render_kw={"placeholder": "Informe a sua senha"})
-    submit = SubmitField('Enviar')
+class NameForm(FlaskForm):
+    name = StringField("Qual é o nome do curso?", validators = [DataRequired()])
+    descricao = TextAreaField('Descrição (250 caracteres)', validators = [DataRequired(), Length(min=1, max=250, message="O texto deve ter entre 1 e 250 caracteres.")])
+    submit = SubmitField('Submit')
 
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/cursos', methods=['GET','POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user_role = Role.query.filter_by(name = form.role.data).first()
-            user = User(username=form.name.data, role = user_role)
-            db.session.add(user)
+        curso = Curso.query.filter_by(nome=form.name.data).first()
+        if curso is None:
+            curso = Curso(nome=form.name.data, descricao=form.descricao.data)
+            db.session.add(curso)
             db.session.commit()
             session['known'] = False
         else:
             session['known'] = True
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    usuarios = User.query.all()
-    funcoes = Role.query.order_by(Role.name).all()
-    qtdUsuarios = User.query.count()
-    qtdFuncoes = Role.query.count()
-    return render_template('index.html', form=form, nome=session.get('name'), known=session.get('known', False), usuarios = usuarios, funcoes = funcoes, qtdUsuarios = qtdUsuarios, qtdFuncoes = qtdFuncoes)
+    cursos = Curso.query.order_by(Role.name).all()
+    return render_template('cadastroCursos.html', form=form, nome=session.get('name'), known=session.get('known', False), cursos=cursos)
 
-@app.route('/login', methods=['GET','POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        session['usuario'] = form.usuario.data
-        return redirect(url_for('loginResponse'))
-    return render_template('login.html', form = form)
-
-@app.route('/loginResponse')
-def loginResponse():
-    return render_template('loginResponse.html', usuario = session.get('usuario'))
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', nome=name)
-
-@app.route('/user/')
-def userr():
+@app.route('/')
+def index():
     return render_template('user.html')
-
-@app.route('/rotainexistente')
-def rotainexistente():
-    return render_template('404.html')
-
-@app.route('/user/<nome>/<prontuario>/<instituicao>')
-def identificacao(nome, prontuario, instituicao):
-    return render_template('user.html', nome=nome, prontuario=prontuario, instituicao=instituicao)
-
-@app.route('/contextorequisicao/<nome>')
-def contextorequisicao(nome):
-    requisicao = request.headers.get('User-Agent')
-    IP = request.remote_addr
-    host = request.host
-    return render_template('contextorequisicao.html', nome=nome, requisicao=requisicao, IP=IP, host=host)
